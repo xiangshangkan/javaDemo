baeb665bc7c3fa257da0e946f45a609213b8e478
xiangshangkan <1940627783@qq.com>
2018-09-11 09:39:05 +0800

    IntegerDemo1

---
+++ src/javazh/langzh/IntegerDemo.java
@@ -0,0 +1,19 @@
+package javazh.langzh;
+
+
+/**
+ * Created by Administrator on 2018/9/11.
+ */
+public class IntegerDemo {
+
+    public void IntegerDemo1(){
+        Integer a = 1;
+        Integer b = 1;
+        Integer c = 129;
+        Integer d = 129;
+
+        System.out.println(a +":" + (a == b));
+        System.out.println(c == d);
+        System.out.println(c == 129);
+    }
+}
