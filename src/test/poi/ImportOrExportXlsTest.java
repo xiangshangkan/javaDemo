1028217d2122417904b2b3085bdb7e70eb95a811
xiangshangkan <1940627783@qq.com>
2018-12-02 21:52:40 +0800

    POIexcel数据读取

---
+++ src/test/poi/ImportOrExportXlsTest.java
@@ -0,0 +1,34 @@
+package test.poi;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.junit.Test;
+import poi.ImportOrExportXls;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * xls格式文件导入导出测试
+ */
+public class ImportOrExportXlsTest {
+
+    private ImportOrExportXls importOrExportXls = new ImportOrExportXls();
+
+
+    @Test
+    public void importTest(){
+        String path = "C:\\Users\\zh\\Desktop\\测试.xls";
+        try {
+            InputStream is = new FileInputStream(path);
+            HSSFWorkbook hwk = new HSSFWorkbook(is);
+            importOrExportXls.importXls(hwk);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
